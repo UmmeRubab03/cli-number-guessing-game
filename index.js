c722cfb72cc8b765f14356e17be6d81d3bce6266
UmmeRubab03 <ilyasqureshi197@gamil.com>
2024-05-22 22:22:37 +0500

    push number guessing game

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) Computer will be generate a random number
+// 2) User input for guessing number
+// 3) Compare user input with computer generated number
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "Please guess a number between 1-6:",
+        type: "number",
+        name: "userInput",
+    },
+]);
+if (answer.userInput === randomNumber) {
+    console.log("Congratulation! You guessed a right number.");
+}
+else {
+    console.log("Opps! You guessed a wrong number.");
+}
+;
